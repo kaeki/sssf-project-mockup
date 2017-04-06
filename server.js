11d9f70091cb56154f9755500984d27f033438f8
kaeki <attesalo@msn.com>
2017-04-06 10:58:09 +0300

    added redirecting to https

--- server.js
+++ server.js
@@ -3,6 +3,24 @@ const app = express();
 // To share files from public folder
 app.use(express.static('public'));
 
+const express = require('express');
+const app = express();
+
+app.enable('trust proxy');
+
+// Add a handler to inspect the req.secure flag (see 
+// http://expressjs.com/api#req.secure). This allows us 
+// to know whether the request was via http or https.
+// https://github.com/aerwin/https-redirect-demo/blob/master/server.js
+app.use ((req, res, next) => {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 
 
 app.listen(5000, () => {
